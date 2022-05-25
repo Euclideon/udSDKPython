c4d05959e9c4a92e1b5736aebea9f6f93cc4cb42
Braden Wockner <bwockner@euclideon.com>
2022-05-25 11:06:21 +1000

    added halfspace, CSG and inverse to udGeometry

--- src/udSDK/udSDKGeometry.py
+++ src/udSDK/udSDKGeometry.py
@@ -1,3 +1,4 @@
+import ctypes
 from ctypes import *
 from enum import IntEnum, unique
 
@@ -20,6 +21,12 @@ class udGeometryType(IntEnum):
   udGT_CSG = 11  # //!< A constructed solid geometry that uses a udGeometryCSGOperation to join to child udGeometry nodes
 
   udGT_Count = 12  # /!< Count helper value to iterate this enum
+
+@unique
+class udGeometryCSGOperation(IntEnum):
+  udCSGO_Union = 0
+  udCSGO_Difference = 1
+  udCSGO_Intersection = 2
 
 class udGeometryDouble2(Structure):
   _fields_=[
@@ -53,21 +60,23 @@ class udGeometry():
     self._udGeometry_Create = getattr(udSDK.udSDKlib, "udGeometry_Create")
     self._udGeometry_Destroy = getattr(udSDK.udSDKlib, "udGeometry_Destroy")
     self._udGeometry_Deinit = getattr(udSDK.udSDKlib, "udGeometry_Deinit")
-    self._udGeometry_InitOBB = getattr(udSDK.udSDKlib, "udGeometry_InitOBB")
-    self._udGeometry_InitAABBFromCentreExtents = getattr(udSDK.udSDKlib, "udGeometry_InitAABBFromCentreExtents")
-    self._udGeometry_InitAABBFromMinMax = getattr(udSDK.udSDKlib, "udGeometry_InitAABBFromMinMax")
-    self._udGeometry_InitSphere = getattr(udSDK.udSDKlib, "udGeometry_InitSphere")
-    self._udGeometry_InitCapsule = getattr(udSDK.udSDKlib, "udGeometry_InitCapsule")
-    #self._udGeometry_InitCylinderFromCenterAndHeight = getattr(udSDK.udSDKlib, "_udGeometry_InitCylinderFromCenterAndHeight")
-    #self._udGeometry_InitCylinderFromEndPoints = getattr(udSDK.udSDKlib, "udGeometry_InitCylinderFromEndPoints")
-    #self._udGeometry_InitHalfSpace = getattr(udSDK.udSDKlib, "udGeometry_InitHalfSpace")
-    #self._udGeometry_InitCSG = getattr(udSDK.udSDKlib, "udGeometry_InitCSG")
-    #self._udGeometry_InitPolygonPerspective = getattr(udSDK.udSDKlib, "udGeometry_InitPolygonPerspective")
-    #self._udGeometry_InitPolygonXY = getattr(udSDK.udSDKlib, "udGeometry_InitPolygonXY")
-    #self._udGeometry_InitRectangleXY = getattr(udSDK.udSDKlib, "udGeometry_InitRectangleXY")
-    #self._udGeometry_InitCircleXY = getattr(udSDK.udSDKlib, "udGeometry_InitCircleXY")
-    #self._udGeometry_InitInverse = getattr(udSDK.udSDKlib, "udGeometry_InitInverse")
-    self._udGeometry_Destroy = getattr(udSDK.udSDKlib, "udGeometry_Destroy")
+    self._udGeometry_InitOBB = udSDK.udExceptionDecorator(udSDK.udSDKLib.udGeometry_InitOBB)
+    self._udGeometry_InitAABBFromCentreExtents = udSDK.udExceptionDecorator(udSDK.udSDKlib.udGeometry_InitAABBFromCentreExtents)
+    self._udGeometry_InitAABBFromMinMax = udSDK.udExceptionDecorator(udSDK.udSDKlib.udGeometry_InitAABBFromMinMax)
+    self._udGeometry_InitSphere = udSDK.udExceptionDecorator(udSDK.udSDKlib.udGeometry_InitSphere)
+    self._udGeometry_InitCapsule = udSDK.udExceptionDecorator(udSDK.udSDKlib.udGeometry_InitCapsule)
+
+    # Deprecated:
+    #self._udGeometry_InitCylinderFromCenterAndHeight = udSDK.udExceptionDecorator(udSDK.udSDKlib._udGeometry_InitCylinderFromCenterAndHeight)
+    #self._udGeometry_InitCylinderFromEndPoints = udSDK.udExceptionDecorator(udSDK.udSDKlib.udGeometry_InitCylinderFromEndPoints)
+
+    self._udGeometry_InitHalfSpace = udSDK.udExceptionDecorator(udSDK.udSDKlib.udGeometry_InitHalfSpace)
+    self._udGeometry_InitCSG = udSDK.udExceptionDecorator(udSDK.udSDKlib.udGeometry_InitCSG)
+    self._udGeometry_InitPolygonPerspective = udSDK.udExceptionDecorator(udSDK.udSDKlib.udGeometry_InitPolygonPerspective)
+    self._udGeometry_InitPolygonXY = udSDK.udExceptionDecorator(udSDK.udSDKlib.udGeometry_InitPolygonXY)
+    self._udGeometry_InitRectangleXY = udSDK.udExceptionDecorator(udSDK.udSDKlib.udGeometry_InitRectangleXY)
+    self._udGeometry_InitCircleXY = udSDK.udExceptionDecorator(udSDK.udSDKlib.udGeometry_InitCircleXY)
+    self._udGeometry_InitInverse = udSDK.udExceptionDecorator(udSDK.udSDKlib.udGeometry_InitInverse)
 
     udSDK._HandleReturnValue(self._udGeometry_Create(byref(self.pGeometry)))
 
@@ -81,6 +90,7 @@ class udGeometry():
     currently this is not synchronized so future changes to the filter will not be reflected in the
     node. TODO: make nodes synchronize
     """
+
     ret = parent.create_child(type="QFilter", name="queryFilter")
     ret.coordinates = self.position
     if hasattr(self, "radius"):
@@ -97,7 +107,41 @@ class udGeometry():
         ret.set_metadata_double(f"transform.rotation.{cs[i]}", self.yawPitchRoll[i])
     return ret
 
+  @property
+  def inverse(self):
+    """
+    The inverse of the udGeometry filter
+    """
+    return udGeometryInverse(self)
+
+  def union(self, other):
+    """
+    returns the result of a union set operation between this and another udGeometry object
+    """
+    return udGeometryCSG(self, other, udGeometryCSGOperation.udCSGO_Union)
+
+  def difference(self, other):
+    """
+    returns the result of a difference set operation between this and another udGeometry object
+    """
+    return udGeometryCSG(self, other, udGeometryCSGOperation.udCSGO_Difference)
+
+  def intersection(self, other):
+    """
+    returns the result of an intersection set operation between this and another udGeometry object
+    """
+    return udGeometryCSG(self, other, udGeometryCSGOperation.udCSGO_Intersection)
+
+  def __add__(self, other):
+    return self.union(other)
+
+  def __sub__(self, other):
+    return self.difference(other)
+
 class udGeometrySphere(udGeometry):
+  """
+  Represents a sphere with a radius of radius and a centre at centre
+  """
   def __init__(self, centre, radius):
     super(udGeometrySphere, self).__init__()
     self.__position = centre
@@ -129,7 +173,10 @@ class udGeometrySphere(udGeometry):
     self._set_geometry()
 
 class udGeometryOBB(udGeometry):
-  def __init__(self, position = (0,0,0), size=(1,1,1), yawPitchRoll=(0,0,0)):
+  """
+  Represents an oriented bounding box with a position, size, and orientation
+  """
+  def __init__(self, position=(0, 0, 0), size=(1, 1, 1), yawPitchRoll=(0, 0, 0)):
     super(udGeometryOBB, self).__init__()
     self.__position = position
     self.__size = size
@@ -154,6 +201,9 @@ class udGeometryOBB(udGeometry):
 
   @property
   def position(self):
+    """
+    Position of the centre of the box
+    """
     return self.__position
 
   @position.setter
@@ -163,6 +213,9 @@ class udGeometryOBB(udGeometry):
 
   @property
   def yawPitchRoll(self):
+    """
+    orientation of the box in yaw-pitch-roll (radians)
+    """
     return self.__yawPitchRoll
 
   @yawPitchRoll.setter
@@ -172,12 +225,82 @@ class udGeometryOBB(udGeometry):
 
   @property
   def size(self):
+    """
+    length of the box sides
+    """
     return self.__size
 
   @size.setter
   def size(self, size):
+    assert len(size) == 3
     self.__size = tuple([*size])
     self._set_geometry()
 
 
 
+class udGeometryHalfSpace(udGeometry):
+  """
+  Represents an infinite plane in 3D space defined by a normal and a point it passes through
+  """
+  def __init__(self, point, normal):
+    self._point = point
+    self._normal = normal
+    super(udGeometryHalfSpace, self).__init__()
+
+  def _set_geometry(self):
+    self._udGeometry_Deinit(self.pGeometry)
+    pointC = udGeometryDouble3()
+    pointC.x = self._point[0]
+    pointC.y = self._point[1]
+    pointC.z = self._point[2]
+
+    normalC = udGeometryDouble3()
+    normalC.x = self._normal[0]
+    normalC.y = self._normal[1]
+    normalC.z = self._normal[2]
+    self._udGeometry_InitHalfSpace(self.pGeometry, pointC, normalC)
+
+  @property
+  def point(self):
+    """
+    point that the plane passes through
+    """
+    return self._point
+
+  @point.setter
+  def point(self, value):
+    self._point = value
+    self._set_geometry()
+
+  @property
+  def normal(self):
+    """
+    3D vector normal to the plane
+    """
+    return self._normal
+
+  @normal.setter
+  def normal(self, value):
+    self._normal = value
+    self._set_geometry()
+
+
+class udGeometryInverse(udGeometry):
+  """
+  Inverse of an existing udGeometry object
+  """
+  def __init__(self, source: udGeometry):
+    self._source = source
+    super(udGeometryInverse, self).__init__()
+    self._udGeometry_InitInverse(self.pGeometry, source.pGeometry)
+
+
+class udGeometryCSG(udGeometry):
+  """
+  Constructive Solid Geometry formed by set operations on existing udGeometry objects
+  """
+  def __init__(self, left: udGeometry, right: udGeometry, operation: udGeometryCSGOperation):
+    self._left = left
+    self._right = right
+    super(udGeometryCSG, self).__init__()
+    self._udGeometry_InitCSG(self.pGeometry, left.pGeometry, right.pGeometry, ctypes.c_uint(operation))
