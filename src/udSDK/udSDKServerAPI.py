8bf761967bf9bcef60b6bbc7785574f52a267884
Braden Wockner <bwockner@euclideon.com>
2022-06-01 10:09:13 +1000

    added serverapi doc strings

--- src/udSDK/udSDKServerAPI.py
+++ src/udSDK/udSDKServerAPI.py
@@ -5,12 +5,18 @@ import udSDK
 
 
 class udServerAPI():
+  """
+  Represents an interface for sending and receiving API calls from a udCloud server
+  """
   def __init__(self, context :udSDK.udContext):
     self._udServerAPI_Query = udSDK.udExceptionDecorator(udSDK.udSDKlib.udServerAPI_Query)
     self._udServerAPI_ReleaseResult = udSDK.udExceptionDecorator(udSDK.udSDKlib.udServerAPI_ReleaseResult)
     self.context = context
 
   def query(self, apiAddress:str, request:str):
+    """
+    Send query to apiAddress with a payload of request
+    """
     pResult = ctypes.c_char_p(0)
     if request is not None:
       request = request.encode('utf8')
